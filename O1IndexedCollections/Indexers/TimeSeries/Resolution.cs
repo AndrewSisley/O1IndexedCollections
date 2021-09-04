8436ec888eb65b4dba460ff9988ebb061d58e9dc
Andrew Sisley <a.j.sisley@hotmail.com>
2021-09-04 13:47:21 -0400

    Add Resolution data struct

---
+++ O1IndexedCollections/Indexers/TimeSeries/Resolution.cs
@@ -0,0 +1,9 @@
+namespace O1IndexedCollections.Indexers.TimeSeries {
+    public struct Resolution {
+        public readonly long Ticks;
+
+        public Resolution(long ticks) {
+            Ticks = ticks;
+        }
+    }
+}
